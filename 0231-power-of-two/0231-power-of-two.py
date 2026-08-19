d26b6f5c4608c8cfdaeb68adc5b8482fa8f7faca
malescript34 <malescript34@gmail.com>
2026-08-19 13:06:57 +0530

    Time: 3 ms (15.9%), Space: 12.3 MB (90.22%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n == 1:
+            return True
+
+        elif n % 2 == 1 or n == 0:
+            return False
+
+        while n % 2 == 0:
+            n/=2
+        
+        if n == 1:
+            return True
+        
+        return False
