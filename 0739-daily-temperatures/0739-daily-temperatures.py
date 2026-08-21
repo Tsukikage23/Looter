618f1f426d75e6657f57c22df5a31abb2cde6d0b
malescript34 <malescript34@gmail.com>
2026-08-21 22:38:42 +0530

    Time: 172 ms (39.75%), Space: 27 MB (41.55%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        stack = []
+        n = len(temperatures)
+        ans = [0]*n
+        for i in range(n - 1,-1,-1):
+            while stack and temperatures[stack[-1]] <= temperatures[i]:
+                stack.pop()
+            if stack:
+                ans[i] = stack[-1] - i
+            stack.append(i)
+        return ans
