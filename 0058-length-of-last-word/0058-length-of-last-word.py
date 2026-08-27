f651fd4822102d8da3580dc503d180cc16a09675
malescript34 <malescript34@gmail.com>
2026-08-27 17:39:59 +0530

    Time: 1 ms (9.24%), Space: 12.5 MB (16.2%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -2,13 +2,20 @@ class Solution(object):
     def lengthOfLastWord(self, s):
         i = str()
         a = []
-        for k in range(len(s)):
+        # for k in range(len(s)):
+        #     if s[k] == " ":
+        #         if i:
+        #             a.append(i)
+        #         i = str()
+        #         continue
+        #     i+=s[k]
+        # if i:
+        #     a.append(i)
+        # return len(a[-1])
+        for k in range(len(s)-1,-1,-1):
             if s[k] == " ":
                 if i:
-                    a.append(i)
-                i = str()
-                continue
-            i+=s[k]
-        if i:
-            a.append(i)
-        return len(a[-1])
+                    return len(i)
+            else:
+                i+=s[k]
+        return len(i)
