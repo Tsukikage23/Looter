7152b7bffed13916e87c0003996290bd60eaf7a1
malescript34 <malescript34@gmail.com>
2026-08-27 20:44:32 +0530

    Time: 35 ms (93.85%), Space: 13.1 MB (18.46%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.py
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def doesAliceWin(self, s):
-        c = 0
         for i in s:
-            if i not in "aeiou":
-                c+=1
-        if c == len(s):
-            return False
-        return True
+            if i in "aeiou":
+                return True
+        return False
